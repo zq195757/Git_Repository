574b3996d9f0bcb7fc82775613d93ce04c31ac25
zqdd <zqdd@195757.com>
2021-04-04 15:20:06 +0800

    再次提交工程中的Java类

--- Git_Part/src/Git_Integration/Student.java
+++ Git_Part/src/Git_Integration/Student.java
@@ -5,5 +5,46 @@ public class Student {
     private String name;
     private char sex;
 
+    public Student(){
 
+    }
+
+    public Student(int age, String name, char sex) {
+        this.age = age;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
 }
